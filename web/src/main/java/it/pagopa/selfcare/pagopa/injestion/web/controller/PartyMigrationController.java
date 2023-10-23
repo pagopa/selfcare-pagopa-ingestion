e0bee4b5e26639168eb4d52a214abf5415efb218
BaldiVi <vittorio.baldi@emea.nttdata.com>
2023-10-23 11:52:10 +0200

    Fix code

--- web/src/main/java/it/pagopa/selfcare/pagopa/injestion/web/controller/PartyMigrationController.java
+++ web/src/main/java/it/pagopa/selfcare/pagopa/injestion/web/controller/PartyMigrationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("swagger-resources/migration")
+@RequestMapping("migration")
 @Api(tags = "migration")
 public class PartyMigrationController {
 
